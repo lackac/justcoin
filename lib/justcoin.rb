8864ce5f10df2c6440e707edf9d8a72a0af8ec20
Laszlo Bacsi <lackac@lackac.hu>
2014-08-17 19:02:03 +0200

    Build API client with Faraday

--- lib/justcoin.rb
+++ lib/justcoin.rb
@@ -1,5 +1,29 @@
+require "faraday"
+require "faraday_middleware"
+
 require "justcoin/version"
 
 class Justcoin
-  # Your code goes here...
+
+  DEFAULT_URI = "https://justcoin.com/api/v1"
+
+  attr_reader :key, :options, :client
+
+  def initialize(key, options={})
+    @key = key
+    @options = options
+    @client = build_client
+  end
+
+  private
+
+  def build_client
+    Faraday.new options[:uri] || DEFAULT_URI do |f|
+      f.request :json
+      f.response :json, content_type: /\bjson$/
+      f.response :logger, options[:logger] if options[:logger] || options[:log]
+      f.response :raise_error
+    end
+  end
+
 end
--- spec/justcoin_spec.rb
+++ spec/justcoin_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe Justcoin do
-  it 'should have a version number' do
-    Justcoin::VERSION.should_not be_nil
+
+  it "should have a version number" do
+    expect(Justcoin::VERSION).to_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe '#initialize' do
+    it "builds a client" do
+      justcoin = Justcoin.new("somekey")
+      expect(justcoin.client).to respond_to(:get)
+    end
   end
+
 end
