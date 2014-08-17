0d3ce7353ab51f85324825d9adcc8f4120d2d724
Laszlo Bacsi <lackac@lackac.hu>
2014-08-18 00:11:24 +0200

    Allow passing options to Faraday, set adapter

--- lib/justcoin.rb
+++ lib/justcoin.rb
@@ -5,24 +5,34 @@ require "justcoin/version"
 
 class Justcoin
 
-  DEFAULT_URI = "https://justcoin.com/api/v1"
+  DEFAULT_URL = "https://justcoin.com/api/v1".freeze
 
   attr_reader :key, :options, :client
 
   def initialize(key, options={})
     @key = key
+
+    options[:url] ||= DEFAULT_URL
     @options = options
+
     @client = build_client
   end
 
   private
 
+  def client_options
+    options.select { |k,v| Faraday::ConnectionOptions.members.include?(k) }
+  end
+
   def build_client
-    Faraday.new options[:uri] || DEFAULT_URI do |f|
+    Faraday.new(client_options) do |f|
       f.request :json
+
       f.response :json, content_type: /\bjson$/
       f.response :logger, options[:logger] if options[:logger] || options[:log]
       f.response :raise_error
+
+      f.adapter Faraday.default_adapter
     end
   end
 
